1f8745c401a162aa3875b834ca42e4ce738b4705
pcsmomo <dongwoo_333@hanmail.net>
2021-11-01 17:14:30 +1100

    43. Consuming Store State

--- 04-redux-ts/src/components/RepositoriesList.tsx
+++ 04-redux-ts/src/components/RepositoriesList.tsx
@@ -5,7 +5,7 @@ import { useActions } from '../hooks/useActions';
 const RepositoriesList = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
-  const { data, loading, error } = useTypedSelector(
+  const { data, error, loading } = useTypedSelector(
     (state) => state.repositories
   );
 
@@ -22,6 +22,9 @@ const RepositoriesList = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
