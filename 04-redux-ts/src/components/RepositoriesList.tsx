048c9f596896028446e19ec756beacdad48621b4
pcsmomo <dongwoo_333@hanmail.net>
2021-11-01 16:28:39 +1100

    35. Wiring Up to React

---
+++ 04-redux-ts/src/components/RepositoriesList.tsx
@@ -0,0 +1,12 @@
+const RepositoriesList = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default RepositoriesList;
