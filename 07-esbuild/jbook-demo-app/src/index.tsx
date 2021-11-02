321e777036b8ddd0751eb0d1afda04490661b6be
pcsmomo <dongwoo_333@hanmail.net>
2021-11-02 11:43:16 +1100

    60. Initializing ESBuild

--- 07-esbuild/jbook-demo-app/src/index.tsx
+++ 07-esbuild/jbook-demo-app/src/index.tsx
@@ -1,9 +1,23 @@
-import { useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+
+  const startService = async () => {
+    // old version, esbuild-wasm@0.8.27
+    // const service = await esbuild.startService({
+    const service = await esbuild.initialize({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+    console.log(service);
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
