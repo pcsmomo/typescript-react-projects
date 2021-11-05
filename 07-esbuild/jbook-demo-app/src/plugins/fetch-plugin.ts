4a4e5c49a1e0e1cd227e3b3239d64f9cc95e3eeb
pcsmomo <dongwoo_333@hanmail.net>
2021-11-05 17:33:51 +1100

    93. Separate Load Filters

--- 07-esbuild/jbook-demo-app/src/plugins/fetch-plugin.ts
+++ 07-esbuild/jbook-demo-app/src/plugins/fetch-plugin.ts
@@ -10,14 +10,52 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
         }
 
+        const { data, request } = await axios.get(args.path);
+
+        // Handle CSS data to inline line
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/, "\\'");
+
+        const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escaped}';
+            document.head.appendChild(style);
+          `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // Check to see if we have already fetched this file
         // and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
@@ -33,26 +71,9 @@ export const fetchPlugin = (inputCode: string) => {
         // console.log(request);
         // console.log(new URL('./', request.responseURL).pathname);
 
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
-        // Handle CSS data to inline line
-        const escaped = data
-          .replace(/\n/g, '')
-          .replace(/"/g, '\\"')
-          .replace(/'/, "\\'");
-
-        const contents =
-          fileType === 'css'
-            ? `
-            const style = document.createElement('style');
-            style.innerText = '${escaped}';
-            document.head.appendChild(style);
-          `
-            : data;
-
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: contents,
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
 
