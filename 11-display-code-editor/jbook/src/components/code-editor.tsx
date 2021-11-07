e0c6b063bb21c1472e03baf2e39ad8062fd46ec1
pcsmomo <dongwoo_333@hanmail.net>
2021-11-08 07:11:14 +1100

    122. Configuring the Editor

--- 11-display-code-editor/jbook/src/components/code-editor.tsx
+++ 11-display-code-editor/jbook/src/components/code-editor.tsx
@@ -1,7 +1,7 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height="300px" />;
+  return <MonacoEditor defaultLanguage="javascript" height="300px" />;
 };
 
 export default CodeEditor;
