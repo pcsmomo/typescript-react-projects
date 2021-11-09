ec8c9eac0f10f78bd8ec39d6860b9681e667cfff
pcsmomo <dongwoo_333@hanmail.net>
2021-11-10 06:42:46 +1100

    134. Refactoring Out a Preview Component

--- 12-extract-reusable-components/jbook/src/index.tsx
+++ 12-extract-reusable-components/jbook/src/index.tsx
@@ -5,12 +5,12 @@ import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 const App = () => {
   const ref = useRef<any>(false);
-  const iframe = useRef<any>(false);
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
-  // const [code, setCode] = useState('');
 
   const startService = async () => {
     // old version, esbuild-wasm@0.8.27
@@ -32,8 +32,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     // const result = await esbuild.transform(input, {
     //   loader: 'jsx',
     //   target: 'es2015',
@@ -51,30 +49,8 @@ const App = () => {
       },
     });
 
-    // setCode(result.outputFiles[0].text);
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-              // throw err;
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
@@ -91,12 +67,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       {/* <pre>{code}</pre> */}
-      <iframe
-        title="preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      />
+      <Preview code={code} />
     </div>
   );
 };
