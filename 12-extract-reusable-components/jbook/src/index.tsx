a1f30e239092870735be303cae0a99d23ba5dec5
pcsmomo <dongwoo_333@hanmail.net>
2021-11-10 07:20:41 +1100

    136. Fixing a Few Warnings

--- 12-extract-reusable-components/jbook/src/index.tsx
+++ 12-extract-reusable-components/jbook/src/index.tsx
@@ -25,11 +25,6 @@ const App = () => {
         initialValue="const a = 1;"
         onChange={(value) => setInput(value)}
       />
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        style={{ width: 500, height: 200 }}
-      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
