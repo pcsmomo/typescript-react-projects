ae475dd6b4b373ba3168c6f0a53ed88263b4e7fb
pcsmomo <dongwoo_333@hanmail.net>
2021-11-10 07:17:50 +1100

    135. Extracting Bundling Logic

--- 12-extract-reusable-components/jbook/src/index.tsx
+++ 12-extract-reusable-components/jbook/src/index.tsx
@@ -1,55 +1,22 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import { setupBundler, bundle } from './bundler';
 
 const App = () => {
-  const ref = useRef<any>(false);
   const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    // old version, esbuild-wasm@0.8.27
-    // const service = await esbuild.startService({
-    await esbuild.initialize({
-      worker: true,
-      // wasmURL: '/esbuild.wasm',
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.13.12/esbuild.wasm',
-    });
-    ref.current = true;
-  };
   useEffect(() => {
-    startService();
+    setupBundler();
   }, []);
 
   const onClick = async () => {
-    if (!ref.current) {
-      console.log('ESBuild is not yet initialized');
-      return;
-    }
-
-    // const result = await esbuild.transform(input, {
-    //   loader: 'jsx',
-    //   target: 'es2015',
-    // });
-    // setCode(result.code);
-
-    const result = await esbuild.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
