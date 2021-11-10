a3110c8d576aef18b2b5ffcee4115ac7d663cbff
pcsmomo <dongwoo_333@hanmail.net>
2021-11-11 07:36:42 +1100

    147. Resizing Horizontally

--- 13-draggable-resizing-components/jbook/src/components/code-cell.tsx
+++ 13-draggable-resizing-components/jbook/src/components/code-cell.tsx
@@ -18,11 +18,13 @@ const CodeCell = () => {
   return (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue="const a = 1;"
-          onChange={(value) => setInput(value)}
-        />
-        <Preview code={code} />
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+          <Preview code={code} />
+        </Resizable>
       </div>
     </Resizable>
   );
