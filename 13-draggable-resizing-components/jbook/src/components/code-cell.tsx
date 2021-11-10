9c0c4e0160983da0d9a20213adcb6448b06926cd
pcsmomo <dongwoo_333@hanmail.net>
2021-11-10 17:44:21 +1100

    143. Expanding the Resizer Horizontally

--- 13-draggable-resizing-components/jbook/src/components/code-cell.tsx
+++ 13-draggable-resizing-components/jbook/src/components/code-cell.tsx
@@ -17,15 +17,11 @@ const CodeCell = () => {
 
   return (
     <Resizable direction="vertical">
-      <div>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <CodeEditor
           initialValue="const a = 1;"
           onChange={(value) => setInput(value)}
         />
-        <div>
-          <button onClick={onClick}>Submit</button>
-        </div>
-        {/* <pre>{code}</pre> */}
         <Preview code={code} />
       </div>
     </Resizable>
