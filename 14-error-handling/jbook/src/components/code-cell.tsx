86e2a835b250934d67fea9b304f34bb142e3d5bb
pcsmomo <dongwoo_333@hanmail.net>
2021-11-11 20:55:01 +1100

    copy jbook project

---
+++ 14-error-handling/jbook/src/components/code-cell.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+import Resizable from './resizable';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+
+import { bundle } from '../bundler';
+
+const CodeCell = () => {
+  const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  return (
+    <Resizable direction="vertical">
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} />
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
---
+++ 14-error-handling/jbook/src/index.tsx
@@ -0,0 +1,21 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import CodeCell from './components/code-cell';
+
+import { setupBundler } from './bundler';
+
+const App = () => {
+  useEffect(() => {
+    // Initialise esbuild
+    setupBundler();
+  }, []);
+
+  return (
+    <div>
+      <CodeCell />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
