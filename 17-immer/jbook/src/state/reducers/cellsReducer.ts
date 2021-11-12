123aa615b19076ca31b1b5ac2c30feb3b06442ab
pcsmomo <dongwoo_333@hanmail.net>
2021-11-13 08:05:31 +1100

    195. Swapping Cell Placement

--- 17-immer/jbook/src/state/reducers/cellsReducer.ts
+++ 17-immer/jbook/src/state/reducers/cellsReducer.ts
@@ -27,7 +27,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
 
       state.data[id].content = content;
       return;
-    case ActionType.DELETE_CELL:
+    case ActionType.DELETE_CELL: {
       // 1. delete data
       delete state.data[action.payload];
 
@@ -39,8 +39,23 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       // state.order = state.order.filter(id => id !== action.payload);
 
       return state;
-    case ActionType.MOVE_CELL:
+    }
+    case ActionType.MOVE_CELL: {
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      // validate
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      // swap
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
       return state;
+    }
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     default:
