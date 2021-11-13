d6874c700206efb9d6e2c34868321144aaedbac9
pcsmomo <dongwoo_333@hanmail.net>
2021-11-13 13:39:08 +1100

    199. Back to React

--- 18-binding-react-redux/jbook/src/index.tsx
+++ 18-binding-react-redux/jbook/src/index.tsx
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 
-// import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 import { setupBundler } from './bundler';
 
@@ -17,10 +16,7 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <div>
-        {/* <CodeCell /> */}
-        <TextEditor />
-      </div>
+      <CellList />
     </Provider>
   );
 };
