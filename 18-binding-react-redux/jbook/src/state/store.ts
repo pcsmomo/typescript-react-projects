b47d23fe4887b4489daff93abd82687275be606c
pcsmomo <dongwoo_333@hanmail.net>
2021-11-13 13:53:26 +1100

    203. Cell Type Rendering

--- 18-binding-react-redux/jbook/src/state/store.ts
+++ 18-binding-react-redux/jbook/src/state/store.ts
@@ -3,26 +3,42 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 // For Manual Test
-// import { ActionType } from './action-types';
+import { ActionType } from './action-types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 // Manual Test
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: 'code',
-//   },
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
 
-// store.dispatch({
-//   type: ActionType.INSERT_CELL_BEFORE,
-//   payload: {
-//     id: null,
-//     type: 'text',
-//   },
-// });
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});
 
 // const id = store.getState().cells.order[0];
 
