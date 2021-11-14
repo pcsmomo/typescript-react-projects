aa64ed06732746a6d32285dcd623d9d7497c0446
pcsmomo <dongwoo_333@hanmail.net>
2021-11-14 14:35:04 +1100

    211. Adding New Cells

---
+++ 18-binding-react-redux/jbook/src/components/add-cell.tsx
@@ -0,0 +1,18 @@
+import './add-cell.css';
+import { useActions } from '../hooks/use-actions';
+
+interface addCellProps {
+  nextCellId: string;
+}
+
+const AddCell: React.FC<addCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useActions();
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+    </div>
+  );
+};
+
+export default AddCell;
