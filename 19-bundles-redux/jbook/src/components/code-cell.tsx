c04b9b3d535de782cd5fbacdb0de091112f46c75
pcsmomo <dongwoo_333@hanmail.net>
2021-11-18 08:09:45 +1100

    233. Getting an Initial Bundle

--- 19-bundles-redux/jbook/src/components/code-cell.tsx
+++ 19-bundles-redux/jbook/src/components/code-cell.tsx
@@ -17,10 +17,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
-    createBundle(cell.id, cell.content);
-  }, []);
-
-  useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 750);
@@ -28,6 +28,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
