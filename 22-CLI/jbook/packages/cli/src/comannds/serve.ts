d303f86bd66dd71bc067c1316c2cebdba87c75de
pcsmomo <dongwoo_333@hanmail.net>
2021-11-21 07:09:56 +1100

    271. Communication from the CLI to the API

--- 22-CLI/jbook/packages/cli/src/comannds/serve.ts
+++ 22-CLI/jbook/packages/cli/src/comannds/serve.ts
@@ -1,10 +1,12 @@
 import { Command } from 'commander';
+import { serve } from 'local-api/dist';
 
 export const serveCommand = new Command()
   .command('serve [filename]') // [] is optional
   .description('Open a file for editing')
   .option('-p, --port <number>', 'port to run server on', '4005') // <> is required
-  .action((filename = 'notebook.js', options) => {
+  .action((filename = 'notebook.js', options: { port: string }) => {
     // console.log('Getting ready to serve a file');
     console.log(filename, options);
+    serve(parseInt(options.port), filename, '/');
   });
